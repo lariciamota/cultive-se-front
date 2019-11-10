8751690c0755e44cb85fd680122ce11b717e4b01
lariciamota <lmmc2@cin.ufpe.br>
2019-11-10 14:41:24 -0300

    Updates

---
+++ precache-manifest.779896d857023642385f1a4be74c72c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a418b6fd88a1ff12493eace10216b6a",
+    "url": "/cultive-se-front/index.html"
+  },
+  {
+    "revision": "a1e613387dcfd1ca4056",
+    "url": "/cultive-se-front/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5743537fb9ffeb20cd17",
+    "url": "/cultive-se-front/static/js/2.fc7da35d.chunk.js"
+  },
+  {
+    "revision": "a1e613387dcfd1ca4056",
+    "url": "/cultive-se-front/static/js/main.81c7ff92.chunk.js"
+  },
+  {
+    "revision": "79d3dd3a83d556ad6361",
+    "url": "/cultive-se-front/static/js/runtime-main.e98da09c.js"
+  }
+]);
